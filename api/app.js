0dfdbd19ce27dac1aeef9ea055a0e1bc776860a0
Jeremy <farrjere@isu.edu>
2022-07-07 15:47:46 -0600

    adding in work for module 2

--- api/app.js
+++ api/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+const router = require('./src/router');
+app.use('/', router);
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 app.use('/*', function(request, response){
     response.sendFile(pathToIndex);
 })
